453c924f17636311d8d6a25c63421955e476e082
taaaf <mattia.tafel@gmail.com>
2022-07-14 11:59:58 +0200

    portfolio v0 start
    portfolio v0 start

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let cube;
+let angle=0;
+let canvas;
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+	canvas.style("z-index", "-1");
+
+	cube = loadModel("obj/cube.obj");
+}
+
+function draw() {
+	background(0);
+
+	directionalLight(255,255,255, 0,0,-1);
+
+	rectMode(CENTER);
+	rotateZ(angle);
+	rotateX(angle/0.4);
+	rotateY(angle/2);
+	scale(100);
+
+	noStroke();
+
+	ambientMaterial(255,0,0);
+	model(cube);
+
+	angle += 0.005;
+
+}
